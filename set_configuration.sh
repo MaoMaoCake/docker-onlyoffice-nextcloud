0ba9c173a7f7737d3f0765f49fa99c24c0cf70eb
maomao <tangpansak@gmail.com>
2022-08-16 10:09:08 +0700

    added ssl support for nginx.conf

--- set_configuration.sh
+++ set_configuration.sh
@@ -9,6 +9,11 @@ if ! grep -q "nextcloud-nginx-server" trusted_domain.tmp; then
     docker exec -u www-data nextcloud-app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nextcloud-nginx-server"
 fi
 
+if ! grep -q "cloud.yourdomain.com" trusted_domain.tmp; then
+    TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
+    docker exec -u www-data nextcloud-app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="cloud.yourdomain.com"
+fi
+
 rm trusted_domain.tmp
 
 docker exec -u www-data nextcloud-app-server php occ --no-warnings app:install onlyoffice
